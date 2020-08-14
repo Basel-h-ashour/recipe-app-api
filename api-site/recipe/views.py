22bccea24c39585e951d944f3a294ff924067b59
Basel H. Ashour <basel_h_ashour@hotmail.com>
2020-08-15 00:11:30 +0200

    added list ingredients endpoint and tests

--- api-site/recipe/views.py
+++ api-site/recipe/views.py
@@ -2,9 +2,9 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
-from .serializers import TagSerializer
+from .serializers import TagSerializer, IngredientSerializer
 
 
 class TagViewSet(viewsets.GenericViewSet,
@@ -23,3 +23,16 @@ class TagViewSet(viewsets.GenericViewSet,
     def perform_create(self, serializer):
         """overrides the save and adds user to it"""
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin):
+    """manage ingredient endpoints"""
+    serializer_class = IngredientSerializer
+    queryset = Ingredient.objects.all()
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+
+    def get_queryset(self):
+        """returns objects for authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
